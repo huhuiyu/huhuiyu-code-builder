e1d5c9651fa1020c85037ee0e0942926b5726545
huhuiyu <1069306849@qq.com>
2022-06-21 08:59:42 +0800

    github地址更改

--- push.ps1
+++ push.ps1
@@ -1,4 +1,4 @@
-# github链接地址：git remote add github git@github.com:huhuiyu/huhuiyu.codebuilder.git
+# github链接地址：git remote add github git@github.com:huhuiyu/huhuiyu-code-builder.git
 # 阿里云链接地址：git@code.aliyun.com:DarkKnight/huhuiyu-code-builder.git
 git push
 git push github
