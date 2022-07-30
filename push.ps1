82eea6a339a42dd61aac04b59f5560384adb1c21
huhuiyu <1069306849@qq.com>
2022-07-30 16:23:49 +0800

    阿里云地址修正

--- push.ps1
+++ push.ps1
@@ -1,6 +1,6 @@
 # github =====> git remote add github git@github.com:huhuiyu/huhuiyu-code-builder.git
 # jihulab =====> git remote add jihulab git@jihulab.com:DarkKnight/huhuiyu-code-builder.git
-# aliyun =====> git remote add aliyun git@codeup.aliyun.com:62e1fa673c1b5281cd478191/huhuiyu-code-builder.git
+# aliyun =====> git remote add aliyun git@codeup.aliyun.com:62e1fa673c1b5281cd478191/DarkKnight/huhuiyu-code-builder.git
 git push
 git push jihulab
 git push aliyun
