13a9c2e29b2a533dec71add77fc9ad70cb7f08ed
DarkKnight <1069306849@qq.com>
2023-10-06 14:50:30 +0800

    多仓库提交脚本修改

--- push.ps1
+++ push.ps1
@@ -1,6 +1,9 @@
-# github =====> git remote add github git@github.com:huhuiyu/huhuiyu-code-builder.git
-# jihulab =====> git remote add jihulab git@jihulab.com:DarkKnight/huhuiyu-code-builder.git
-# gitee =====> git remote add gitee git@gitee.com:huhuiyu/huhuiyu-code-builder.git
-git push
+# 多仓库推送
+git pull
+Write-Output "===========拉取完成==========="
 git push jihulab
+Write-Output "===========推送jihulab完成==========="
 git push gitee
+Write-Output "===========推送gitee完成==========="
+git push
+Write-Output "===========推送github完成==========="
